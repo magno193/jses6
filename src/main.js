e12592251068eef826ca072691147f14dabbf593
Alexandre Ferreira <alexmlf93@gmail.com>
2019-10-18 05:20:34 -0300

    async await parte 2

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ const minhaPromise = () => new Promise((resolve, reject) =>
     setTimeout(() => { resolve('OK') }, 2000)
 );
 
-// async function executaPromise(){
-//     console.log(await minhaPromise());
-//     console.log(await minhaPromise());
-// }
-
-const executaPromise = async () => {
+async function executaPromise(){
     console.log(await minhaPromise());
     console.log(await minhaPromise());
 }
+
+// const executaPromise = async () => {
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
 
 executaPromise();
