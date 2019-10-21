837f0972bf21ee54bee113d1d3132d3ecc2ed827
Alexandre Ferreira <alexmlf93@gmail.com>
2019-10-21 07:52:21 -0300

    Criado javascript para tratar da adição de repositórios

--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
 
-class Api {
-    static async getUserInfo(username) {
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        }
-        catch (err) {
-            console.warn('Erro na API');
-        }
+        this.formElement = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers(){
+        // registra os eventos do usuário
+        this.formElement.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(){
+        // não deixa o form com o funcionamento padrão
+        event.preventDefault();
+
+        // adiciona um novo repositório no array
+        this.repositories.push({
+            name:'rocketseat.com.br',
+            description: 'Tire a sua idéia do papel e dê vida à sua startup.',
+            avatar_url= 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url= 'http://github.com/rocketseat/'
+        });
+
+        console.log(this.repositories);
         
     }
 }
-Api.getUserInfo('diego3g');
+// Para executar a classe
+new App();
