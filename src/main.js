78fdbae10079c2cf64f939dd9cc3d471032a2dd6
Alexandre <alexmlf@hotmail.com>
2019-10-23 00:37:31 -0300

    Renderizando os elementos adicionados na lista

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formElement = document.getElementById('repo-form');
+        this.listElement = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
@@ -23,9 +24,35 @@ class App {
             avatar_url= 'https://avatars0.githubusercontent.com/u/28929274?v=4',
             html_url= 'http://github.com/rocketseat/'
         });
+        this.render();
+    }
+    
+    //apaga todo o conteudo da lista e renderiza do zero
+    render(){
+        this.listElement.innerHTML = ''; // apaga
+        // para percorrer no array e realizar
+        // operacoes para adicionar os itens em index
+        this.repositories.forEach(repo => {
 
-        console.log(this.repositories);
-        
+            let imagemElement = document.createElement('img')
+            imagemElement.setAttribute('src', repo.avatar_url);
+
+            let titleElement = document.createElement('strong');
+            titleElement.appendChild(document.createTextNode(repo.name));
+
+            let descriptionElement = document.createElement('p');
+            descriptionElement.appendChild(document.createTextNode(repo.description));
+                
+            let linkElement = document.createElement('a');
+            linkElement.setAttribute('target', '_blank');
+            linkElement.appendChild(document.createTextNode('Acessar'));
+
+            let listItemElement = document.createElement('li');
+            listItemElement.appendChild(imagemElement);
+            listItemElement.appendChild(titleElement);
+            listItemElement.appendChild(descriptionElement);
+            listItemElement.appendChild(linkElement);
+        })
     }
 }
 // Para executar a classe
