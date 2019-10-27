b2121c1549aabba49e2fd8399aa64d5eee523276
Alexandre <alexmlf@hotmail.com>
2019-10-27 17:03:28 -0300

    consertando get

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import api from './api'
+
 class App {
     constructor() {
         this.repositories = [];
 
         this.formElement = document.getElementById('repo-form');
         this.listElement = document.getElementById('repo-list');
+        this.inputElement = document.querySelector('input[name=repository]')
 
         this.registerHandlers();
     }
@@ -13,16 +16,25 @@ class App {
         this.formElement.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event){
+    async addRepository(event){
         // não deixa o form com o funcionamento padrão
         event.preventDefault();
+
+        const repoInput = this.inputElement.value;
+
+        if (repoInput.length === 0){return;}
+
+        const response = await api.get(`/repos/${repoInput}`);
+
+        console.log(response);
+        
 
         // adiciona um novo repositório no array
         this.repositories.push({
             name:'rocketseat.com.br',
             description: 'Tire a sua idéia do papel e dê vida à sua startup.',
-            avatar_url= 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url= 'http://github.com/rocketseat/'
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/'
         });
         this.render();
     }
